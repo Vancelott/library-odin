4a9dbb9f706ff2c242ad6c482dc4d89bac09d824
Vancelott <ivan.angelovv2@gmail.com>
2023-04-18 11:34:55 +0300

    added a few more functions

--- script.js
+++ script.js
@@ -2,27 +2,17 @@ let myLibrary = [];
 const addBook = document.querySelector("button")
 const submitBook = document.getElementById("submit")
 const form = document.createElement("form")
-
-function Book(title, author, total, read) {
-        this.title = title;
-        this.author = author;
-        this.total = total;
-        this.read = read;
-}
-
-// myLibrary.forEach(Book()) {
-
-// }
+var title = document.createElement("input") 
+var author = document.createElement("input")
+var total = document.createElement("input")
+var read = document.createElement("input")
 
 addBook.addEventListener("click", function() {
+    if(form.length < 4) {
     form.setAttribute('class', 'form')
-        var title = document.createElement("input") 
         title.setAttribute("placeholder", "Book title")
-            var author = document.createElement("input")
             author.setAttribute("placeholder", "Author")
-                var total = document.createElement("input")
                 total.setAttribute("placeholder", "Total pages")
-                    var read = document.createElement("input")
                     read.setAttribute("placeholder", "Pages read")
                         form.appendChild(title)
                         form.appendChild(author)
@@ -30,18 +20,52 @@ addBook.addEventListener("click", function() {
                         form.appendChild(read);
     document.querySelector("body").appendChild(form);
     form.appendChild(submitBook)
+    }
 });
 
-var submitBtn = document.getElementById("submit").style;
+addBook.addEventListener("click", function() {
+    form.style.display = "flex"
+});
 
 addBook.addEventListener("click", function() {
     submitBtn.display = "block"
 });
 
+var submitBtn = document.getElementById("submit").style;
+
+submitBook.addEventListener("click", function(){
+    form.style.display = "none"
+});
 
 submitBook.addEventListener("click", function() {
+    const book = document.createElement("div");
+    book.setAttribute("class", 'addedBook')
+    var li = document.createElement("li");
+    document.querySelector("body").appendChild(book)
 
+    const inputValues = [title.value, author.value, total.value, read.value];
+    for (let i = 0; i < inputValues.length; i++) {
+      const textNode = document.createTextNode(inputValues[i]);
+      li.appendChild(textNode);
+  
+      if (i < inputValues.length - 1) {
+        const br = document.createElement("br");
+        li.appendChild(br);
+      }
+    }  
+    book.appendChild(li)
 });
+
+// function Book(title, author, total, read) {
+//         this.title = title;
+//         this.author = author;
+//         this.total = total;
+//         this.read = read;
+// }
+
+// myLibrary.forEach(Book()) {
+
+// }
 
 // function showSubmit() {
 //     var x = document.querySelector("submit");
